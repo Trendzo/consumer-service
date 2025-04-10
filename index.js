68c142cf4ffbe9c49db1c8d9d79767266a0dc90b
ishankites <ishan@thekites.ai>
2025-04-10 16:04:38 +0530

    1st commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Welcome to the Trendzo Backend!');
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
